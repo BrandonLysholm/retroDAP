f2f978fe9b81547312dfa05521a7d2c4a94b177e
BrandonLysholm <totallyafakeemailforspam@gmail.com>
2025-02-27 11:44:09 -0600

    Created rotaryTest.py, which reads basic input from encoder but not in a meaningful way -Brandon

---
+++ clickwheelTests/rotaryTest.py
@@ -0,0 +1,15 @@
+# This program detects when the rotary encoder starts spinning, and it will read as spinning
+# until it gets spun one click the other direction
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(33, GPIO.IN, GPIO.PUD_UP)
+
+while True:
+    if GPIO.input(33) == GPIO.HIGH:
+        print("Spinning")
+        time.sleep(1)
+        print("end sleep")
