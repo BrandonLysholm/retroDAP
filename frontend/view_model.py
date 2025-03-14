8e418fbe6d98de2acb35c8c149b8bff4827e92e5
BrandonLysholm <totallyafakeemailforspam@gmail.com>
2025-03-13 20:29:45 -0600

    changed all playback commands to the start_playback command in view_model.py -Brandon

--- frontend/view_model.py
+++ frontend/view_model.py
@@ -478,7 +478,7 @@ class SingleTrackPage(MenuPage):
         print("render track")
         context_uri = self.playlist.uri if self.playlist else self.album.uri
         # TODO: Modifying this to get it working
-        spotify_manager.start_playback(DEVICE_ID, context_uri, self.track.uri)
+        spotify_manager.start_playback(DEVICE_ID, context_uri, self.track.uri, None)
         #spotify_manager.play_from_playlist(context_uri, self.track.uri, None)
         return r
 
