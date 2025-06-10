1625237b2fe2864f83cb7058fb96de3520a5e7ea
Brandon <brandon@localhost-live.gateway.sasktel.net>
2025-06-09 20:02:37 -0600

    added app.redraw to my screen_refresh function -Brandon

--- frontend/spotifypod.py
+++ frontend/spotifypod.py
@@ -186,7 +186,8 @@ def screen_refresh():
     # calling the screen to wakeup, which may cause it to refresh
     if screen_on:
         #screen_sleep()
-        app.update()
+        # app.update()
+        app.redraw()
         render(app, page.render())
         screen_wake() 
     # recursive call, so that this gets called every 5 seconds
