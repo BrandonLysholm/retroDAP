0616a609961fdd94bb3999451b58debdfde06441
Brandon <BrandonLysholm@gmail.com>
2025-09-19 21:39:03 -0600

    fixed minor typo -Brandon

--- frontend/view_model.py
+++ frontend/view_model.py
@@ -410,7 +410,7 @@ class PowerPage(SettingsPage):
         super().__init__("Power Off", previous_page, has_sub_page=False)
     #TODO: Add command so that when this is loaded, it runs sudo shutdown and displays shutting down
 
-class WifiPage(SettingsPage)
+class WifiPage(SettingsPage):
     def __init__(self, previous_page)
         super().__init__("WiFi Settings", previous_page, has_sub_page=True)
 
